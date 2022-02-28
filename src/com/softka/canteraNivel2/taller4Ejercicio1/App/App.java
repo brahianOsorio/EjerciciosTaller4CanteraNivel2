a07962c817d49572e18f95c0c798e95f3c7bd9e0
brahianOsorio <96633123+brahianOsorio@users.noreply.github.com>
2022-02-28 15:23:06 -0500

    creación del repositorio y inicio del taller
    Este es el inicio de el taller de buenas practicas en java ejercicio individual

---
+++ src/com/softka/canteraNivel2/taller4Ejercicio1/App/App.java
@@ -0,0 +1,10 @@
+package com.softka.canteraNivel2.taller4Ejercicio1.App;
+
+public class App {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ src/com/softka/canteraNivel2/taller4Ejercicio1/domain/Planeta.java
@@ -0,0 +1,5 @@
+package com.softka.canteraNivel2.taller4Ejercicio1.domain;
+
+public class Planeta {
+
+}
